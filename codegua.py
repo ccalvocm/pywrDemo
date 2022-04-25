6ca06bb8a2e798b453ce47087ae379dc368f2874
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2022-04-24 23:15:26 -0400

    add: Codegua

---
+++ codegua.py
@@ -0,0 +1,22 @@
+from pywr.core import Model
+from pywr.recorders import Recorder
+from pywr.recorders._recorders import NodeRecorder
+import pandas
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    m = Model.load("codegua.json")
+    stats = m.run()
+    print(stats)
+
+    print(m.recorders["turbine1_energy"].values())
+
+    df = m.to_dataframe()
+    print(df.head())
+
+    from matplotlib import pyplot as plt
+
+    df.plot(subplots=True)
+    plt.show()
